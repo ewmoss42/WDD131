ca2832a6fb2f5ffd71dd57eb670ac7129647b6d5
ewmoss42 <ewmoss@byui.edu>
2025-02-07 10:56:04 -0700

    java updated

--- blog/blog.js
+++ blog/blog.js
@@ -2,13 +2,12 @@ const articles = [
 	{
 		id: 1,
 		title: 'Septimus Heap Book One: Magyk',
-		date: 'July 5, 2022',
-		description:
-			'If you enjoy stories about seventh sons of seventh sons and magyk this is the book for you.',
+		date: '2022-07-05', // ISO 8601 format for better sorting and formatting
+		description: 'If you enjoy stories about seventh sons of seventh sons and magyk, this is the book for you.',
 		imgSrc: 'https://upload.wikimedia.org/wikipedia/en/5/5f/Magkycover2.jpg',
 		imgAlt: 'Book cover for Septimus Heap 1',
 		ages: '10-14',
 		genre: 'Fantasy',
-		stars: '****'
+		stars: 4 // Changed from '****' to a number for flexibility
 	},
-]
+];
